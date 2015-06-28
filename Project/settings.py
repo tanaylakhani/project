97f2b4c64450f8f1b0814465e9eb172eda36f1ee
tanaylakhani <tpl253@nyu.edu>
2015-06-28 17:06:13 -0400

    static

--- Project/settings.py
+++ Project/settings.py
@@ -86,10 +86,11 @@ USE_I18N = True
 
 USE_L10N = True
 
+
 USE_TZ = True
 
-
+PROJECT_PATH = BASE_DIR
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = os.path.join(os.path.join(PROJECT_PATH, "TweetMap"), "static")
+STATIC_ROOT = os.path.join(os.path.join(PROJECT_PATH, "Project"), "static")
 STATIC_URL = '/static/'
